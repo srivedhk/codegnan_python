31e1d570e23b012491087aa229609f181543cdd9
Srivedh Kollipara <srivedhk19@gmail.com>
2026-09-16 19:48:02 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,41 @@
+# Python Data Types Example
+
+# Integer
+age = 18
+print("Integer:", age)
+print("Type:", type(age))
+
+# Float
+height = 5.8
+print("Float:", height)
+print("Type:", type(height))
+
+# String
+name = "Srivedh"
+print("String:", name)
+print("Type:", type(name))
+
+# Boolean
+is_student = True
+print("Boolean:", is_student)
+print("Type:", type(is_student))
+
+# List
+marks = [85, 90, 95]
+print("List:", marks)
+print("Type:", type(marks))
+
+# Tuple
+coordinates = (10, 20)
+print("Tuple:", coordinates)
+print("Type:", type(coordinates))
+
+# Set
+numbers = {1, 2, 3}
+print("Set:", numbers)
+print("Type:", type(numbers))
+
+# Dictionary
+student = {"name": "Srivedh", "age": 18}
+print("Dictionary:", student)
+print("Type:", type(student))
